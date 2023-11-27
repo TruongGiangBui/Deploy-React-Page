dd561dbfe53721202158bca19750f03f2d1507ad
Truong Giang <giang.bui@fssc.com.vn>
2023-11-28 00:03:40 +0700

    Updates

---
+++ precache-manifest.6fdccb6b4abb1ab05b949a6081213a8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "254c295d2b675b7fe3deddcc2716eb66",
+    "url": "/HD-Book/index.html"
+  },
+  {
+    "revision": "2eea305ccb2d67cae9b5",
+    "url": "/HD-Book/static/css/2.f382e0e2.chunk.css"
+  },
+  {
+    "revision": "d02835ff0129377e6242",
+    "url": "/HD-Book/static/css/main.cff95a08.chunk.css"
+  },
+  {
+    "revision": "2eea305ccb2d67cae9b5",
+    "url": "/HD-Book/static/js/2.1ce5b219.chunk.js"
+  },
+  {
+    "revision": "d02835ff0129377e6242",
+    "url": "/HD-Book/static/js/main.9f895172.chunk.js"
+  },
+  {
+    "revision": "9b1307b50d02604c00c9",
+    "url": "/HD-Book/static/js/runtime~main.a39d1a6c.js"
+  },
+  {
+    "revision": "b2e97f9c5ac29d027f46a89d4ce378ad",
+    "url": "/HD-Book/static/media/NEW_WATERMARK_001.b2e97f9c.png"
+  }
+]);
